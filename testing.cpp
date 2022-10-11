21157abe6240c990f7b96a78cc4be3a1cfffa239
mrudul2019 <mrudulmnair@gmail.com>
2022-10-11 23:17:42 +0530

    Saving Progress

--- testing.cpp
+++ testing.cpp
@@ -9,6 +9,8 @@ long long int LIMIT = 1e10;
 int main(){
     int t = 0;
     while(LIMIT--)t++;
+    while(LIMIT--)t++;
+    while(LIMIT--)t++;
     cout<<t<<"\n";
     cout<<"Hellooo!\n";
     cout<<"Hellooo!\n";
