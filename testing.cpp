71b1ba15d212d803e71180ff4de195fe81ea01d5
mrudul2019 <mrudulmnair@gmail.com>
2022-10-11 20:32:52 +0530

    Saving Progress

--- testing.cpp
+++ testing.cpp
@@ -5,11 +5,5 @@ using namespace std;
 
 int main(){
     cout<<"Hellooo!\n";
-    cout<<"Testing git\n";
-    cout<<"Testing git\n";
-    cout<<"Yay! Working...\n";
-    cout<<"Auto Save working?\n";
-    cout<<"auto run on save working?\n";
-    cout<<"auto run on save working?\n";
-    cout<<"UI changed..\n";
+    
 }
